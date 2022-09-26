33a401a337911e2785e42d1c835b5e909610f061
Daniel Shao <sxy799@mail.ustc.edu.cn>
2022-09-26 10:56:49 +0800

    Lunule v2.0: treat delta load as spatial load

--- src/mds/adsl/predictors/lunule.lua
+++ src/mds/adsl/predictors/lunule.lua
@@ -10,8 +10,12 @@ function temporal_predict(load_list)
 		cur = load_list[i]
 		s = DECAY_FAC * (_1_DECAY_FAC * cur + s)
 	end
-	s = (1 + DECAY_FAC) * load_list[#load_list] - s
-	return s > 0 and s or 0.0
+	last = load_list[#load_list]
+	d = DECAY_FAC * last - s
+	s = last + d
+	s = s > 0 and s or 0.0
+	d = d > (-last) and d or (-last)
+	return s, d
 end
 
 function predict()
@@ -53,19 +57,21 @@ function predict()
 	local all_hits = new_hits + old_hits
 	local alpha = (all_hits > 0) and (old_hits / all_hits) or 0.1
 	local beta = (subtree_size > 0) and ((subtree_size - num_of_old_entries) / subtree_size) or 0.1
+	print(alpha, beta)
 	if alpha < 0.1 then alpha = 0.1 end
 	if beta < 0.1 then beta = 0.1 end
 
-	-- Sum up all load (uniformly distribution)
-	local last_epoch_total = 0
+	-- Sum up all delta load (uniformly distribution)
+	local last_epoch_delta_total = 0
 	for entry, entry_load_list in ipairs(load_matrix) do
-		last_epoch_total = last_epoch_total + entry_load_list[#entry_load_list]
+		local temporal_load, temporal_delta = temporal_predict(entry_load_list)
+		last_epoch_delta_total = last_epoch_delta_total + temporal_delta
+		prediction[entry] = temporal_load
 	end
-	local spatial_load_each = last_epoch_total / subtree_size
+	local spatial_load_each = last_epoch_delta_total / subtree_size
 
 	for entry, entry_load_list in ipairs(load_matrix) do
-		local temporal_load = temporal_predict(entry_load_list)
-		local calculated = alpha * temporal_load + beta * spatial_load_each
+		local calculated = alpha * prediction[entry] + beta * spatial_load_each
 		prediction[entry] = (calculated > 0) and calculated or 0
 	end
 	return prediction
